4fa1f5176d6f7060e5b4200992d6ddb10c99af77
Vladimir Tuñoque <vvladimirtm134@gmail.com>
2025-03-17 16:27:46 -0500

    actualizar repositorio
    * Implementar funcionalidad en app.js
    * Crear README del proyecto

---
+++ README.md
@@ -0,0 +1,13 @@
+# Challenge Amigo Secreto
+> Este es un pequeño proyecto de javascript que permite sortear una lista de amigos para definir quién será el amigo secreto.
+>
+> Este challenge fue definido por Alura Latam en el programa ONE - G8.
+
+## Funcionalidades:
+* Los usuarios pueden agregar nombres de sus amigos.
+* Los usuarios pueden ver los nombres ingresados en una lista.
+* Se valida que los nombres ingresados no estén en blanco.
+* Se valida que los nombres ingresados no se repitan.
+* El amigo sorteado se muestra en la pantalla y se elimina de la lista.
+* La entrada se limpia cada vez que se presiona el botón de *agregar amigo*
+
--- app.js
+++ app.js
@@ -1,1 +1,67 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    if (verificarNombre(amigo)) {
+        agregarNombre(amigo);
+    }
+    limpiarCampoAmigo();
+}
+
+function agregarNombre(nombre) {
+    amigos.push(nombre);
+    actualizarLista();
+}
+
+function actualizarLista() {
+    listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    amigos.forEach(amigo => {
+        const item = document.createElement("li");
+        item.textContent = amigo;
+        listaAmigos.appendChild(item);
+    })
+}
+
+function limpiarCampoAmigo() {
+    document.getElementById("amigo").value = "";
+}
+
+
+function verificarNombre(nombre) {
+    if (!nombre) {
+        mostrarError("El nombre no puede estar vacío");
+        return false;
+    }
+
+    if (amigos.includes(nombre)) {
+        mostrarError("El nombre ya está incluido en la lista")
+        return false;
+    }
+
+    return true;
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        mostrarError("No hay amigos para sortear");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    amigos.splice(indiceAleatorio, 1);
+    actualizarLista();
+    actualizarResultado(amigoSorteado);
+}
+
+function actualizarResultado(nombre) {
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo secreto es: ${nombre}`;
+}
+
+function mostrarError(mensaje) {
+    document.getElementById("error").innerHTML = mensaje;
+}
